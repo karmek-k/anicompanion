e93aee5195e8121cb2b5619b2714229d31ad6ff1
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-27 13:49:52 +0100

    Remove storage from app context

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,12 +1,9 @@
-import { Storage } from '@ionic/storage';
 import React from 'react';
 
 export interface ContextValues {
-  store: Storage;
   username: string;
 }
 
 export default React.createContext<ContextValues>({
-  store: new Storage(),
   username: ''
 });
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,21 +1,11 @@
 import { IonText } from '@ionic/react';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import PageLayout from '../components/PageLayout';
 
 import AniCompanionContext from '../Context';
 
 const List: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-
-  const { store } = useContext(AniCompanionContext);
-
-  useEffect(() => {
-    async function load() {
-      setUsername(await store.get('username'));
-    }
-
-    load();
-  });
+  const { username } = useContext(AniCompanionContext);
 
   return (
     <PageLayout title="List">
