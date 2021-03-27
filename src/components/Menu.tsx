2e561654fcc445915f252fad7193de5c605ad652
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-27 18:35:58 +0100

    Load user data in Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,15 @@
+import { useQuery } from '@apollo/client';
 import {
   IonContent,
   IonHeader,
+  IonImg,
   IonMenu,
   IonTitle,
   IonToolbar
 } from '@ionic/react';
+import gql from 'graphql-tag';
+import { useContext } from 'react';
+import Context from '../Context';
 
 import './Menu.css';
 
@@ -12,7 +17,25 @@ interface Props {
   title: string;
 }
 
+const userDataQuery = gql`
+  query UserData($id: Int!) {
+    User(id: $id) {
+      avatar {
+        medium
+      }
+      name
+      unreadNotificationCount
+    }
+  }
+`;
+
 const Menu: React.FC<Props> = props => {
+  const { userId } = useContext(Context);
+
+  const { data, loading } = useQuery(userDataQuery, {
+    variables: { id: userId }
+  });
+
   return (
     <IonMenu contentId="main" type="overlay">
       <IonHeader>
@@ -20,7 +43,9 @@ const Menu: React.FC<Props> = props => {
           <IonTitle>{props.title}</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent></IonContent>
+      <IonContent>
+        {!loading && <IonImg src={data.User.avatar.medium} />}
+      </IonContent>
     </IonMenu>
   );
 };
