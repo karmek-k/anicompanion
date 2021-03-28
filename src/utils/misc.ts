ca2f8bf0dadbff7a73377d16b2e09196090b211c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-28 16:23:47 +0200

    Add replaceBr function

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,7 @@
 export function capitalizeString(s: string): string {
   return s.toLowerCase().replace(/^\w/, c => c.toUpperCase());
 }
+
+export function replaceBr(source: string, characterToInsert: string) {
+  return source.replaceAll(/<br( ?\/)?>/g, characterToInsert);
+}
